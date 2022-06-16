9dd3031d76a47a89c6ed9c4e8538a10ceb5f80c2
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-16 13:32:35 +0100

    update

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ module "cassandra" {
   source                 = "github.com/hybridaccess/tf-module-infra.git?ref=master"
   environment            = var.environment
   azurerm_resource_group = data.azurerm_resource_group
-  lb_frontend_ip         = var.frontend_ip
+  frontend_ip            = var.frontend_ip
   name                   = var.cassandra_cluster_name
   location               = var.location
   cassandra_vnet_id      = data.azurerm_virtual_network.nonprod.id
   cassandra_subnet_id    = data.azurerm_subnet.nonprod.id
-  node_sku               = var.cassandra_node_sku
+  cassandra_node_sku     = var.cassandra_node_sku
 }
 
 
