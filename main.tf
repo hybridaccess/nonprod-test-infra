894a4f6f0b6b3cbe46e2ee93f401c565035369dd
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-07-04 00:21:00 +0100

    update module attribute

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ module "cassandra" {
 }
 
 module "ise" {
-  source              = "github.com/hybridaccess/tf-module-azure-ise.git?ref=master"
-  resource_group_name = azurerm_resource_group.this.name
-  name                = "hax-ise"
-  location            = var.location
-  virtual_network_subnet_ids = [
+  source                 = "github.com/hybridaccess/tf-module-azure-ise.git?ref=master"
+  azurerm_resource_group = azurerm_resource_group.this.name
+  name                   = "hax-ise"
+  location               = var.location
+  vnet_subnet_ids = [
     azurerm_subnet.isesubnet1.id,
     azurerm_subnet.isesubnet2.id,
     azurerm_subnet.isesubnet3.id,
