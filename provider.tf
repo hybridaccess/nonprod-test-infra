b5378e334b958524010158e375b935011385218d
Christopher Orisawayi <chrisorisawayi@users.noreply.github.com>
2023-06-18 22:03:18 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "hybridaccessterraform"
     container_name       = "tfstate"
-    key                  = "cassandra.tfstate"
+    key                  = "infra.tfstate"
     subscription_id      = "02b44a0d-f98e-407e-b0c1-7d394305857e"
     tenant_id            = "c555cdfa-e1fa-4957-b451-1ff2ce91d0a7"
     resource_group_name  = "terraform-bootstrap"
